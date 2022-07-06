10b3d31fdf0dace4692a668efd37dfe01e4f7b0a
kangbyounghoon <fapchino@ibrogroup.co.kr>
2022-07-06 14:39:33 +0900

    Catalogs 서비스에 Hystrix 적용

--- Catalogs/src/main/java/egovframework/msa/sample/CatalogsApplication.java
+++ Catalogs/src/main/java/egovframework/msa/sample/CatalogsApplication.java
@@ -2,11 +2,13 @@ package egovframework.msa.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 
+@EnableCircuitBreaker
 @ComponentScan("egovframework.*")
 @SpringBootApplication
 public class CatalogsApplication {
--- Catalogs/src/main/java/egovframework/msa/sample/service/impl/CustomerAPIServiceImpl.java
+++ Catalogs/src/main/java/egovframework/msa/sample/service/impl/CustomerAPIServiceImpl.java
@@ -1,5 +1,6 @@
 package egovframework.msa.sample.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import egovframework.msa.sample.service.CustomerAPIService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,9 +14,15 @@ public class CustomerAPIServiceImpl implements CustomerAPIService {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "getCustomerDetailFallback")
     @Override
     public String getCustomerDetail(String customerId) {
 //        return customerId;
         return restTemplate.getForObject("http://localhost:8082/customers/" + customerId, String.class);
+    }
+
+    public String getCustomerDetailFallback(String customerId, Throwable ex) {
+        System.out.println("getCustomerDetailFallback Error : " + ex.getMessage());
+        return "[고객정보 조회가 지연되고 있습니다.]";
     }
 }
