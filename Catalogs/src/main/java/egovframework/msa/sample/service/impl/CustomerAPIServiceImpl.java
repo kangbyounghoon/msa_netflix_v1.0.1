18f6116f8f06b66c6a2e51217bfbaa322ffd0070
kangbyounghoon <fapchino@ibrogroup.co.kr>
2022-07-06 14:18:03 +0900

    Catalogs & Customers 서비스 연동 및 테스트

--- Catalogs/src/main/java/egovframework/msa/sample/service/impl/CustomerAPIServiceImpl.java
+++ Catalogs/src/main/java/egovframework/msa/sample/service/impl/CustomerAPIServiceImpl.java
@@ -2,12 +2,20 @@ package egovframework.msa.sample.service.impl;
 
 import egovframework.msa.sample.service.CustomerAPIService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CustomerAPIServiceImpl implements CustomerAPIService {
 
+    private final RestTemplate restTemplate;
+
+    public CustomerAPIServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public String getCustomerDetail(String customerId) {
-        return customerId;
+//        return customerId;
+        return restTemplate.getForObject("http://localhost:8082/customers/" + customerId, String.class);
     }
 }
